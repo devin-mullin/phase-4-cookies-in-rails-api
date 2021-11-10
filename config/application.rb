8d79c600765f0da4bfa5342564c75c3f0f521cda
Devin Mullin <devin.mullin90@gmail.com>
2021-11-10 10:42:37 -0600

    commit tree be55d6550942e6048b17988b7c225c0bbdc2064c

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Phase4CookiesInRailsApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    
 
     # Configuration for the application, engines, and railties goes here.
     #
